931789296afa705dd7f327b4671228a76099ba1f
zy nscc ubuntu22.04 <1920548152@qq.com>
2023-06-06 22:06:18 +0800

    增加了dmesg的打印

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
  # @Author: zy nscc ubuntu22.04 1920548152@qq.com
  # @Date: 2023-06-06 20:41:35
  # @LastEditors: zy nscc ubuntu22.04 1920548152@qq.com
- # @LastEditTime: 2023-06-06 20:53:13
+ # @LastEditTime: 2023-06-06 22:04:57
  # @FilePath: /export_symbol/run.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
+sudo dmesg -c
+
 sudo rmmod export_symbol_two
 sudo rmmod export_symbol_one
 
@@ -23,3 +25,5 @@ sudo insmod export_symbol_two/export_symbol_two.ko
 
 sudo rmmod export_symbol_two
 sudo rmmod export_symbol_one
+
+sudo dmesg
